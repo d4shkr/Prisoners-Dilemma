7e34c7a9caf42cb4233b89a382fe4c382ecefb10
d4shkr <krasavtseva04@mail.ru>
2024-04-10 01:19:34 +0300

    daamn prison..(

--- app/index.php
+++ app/index.php
@@ -12,16 +12,19 @@
     <!--Linking of jQuery file using CDN-->
     <script src=”https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js”></script> 
 </head>
-<body>
+<body style='background-image:URL(
+        https://sun9-39.userapi.com/impg/FfOklB7i7CIUgBhuOhR2go0d31ztGRTNjASTgQ/xWWTfXqB7Z4.jpg?size=1024x683&quality=96&sign=08e4650c29291d9150aff267e6cc4aca&c_uniq_tag=JyPXdr7Bzq2oSWgQUD2iY31QMpxtwiuKUP8EhYEQ1xk&type=album>);
+        background-repeat: no-repeat; background-size: cover;'>
     <header>
+
     <div class="main-heading">
         <h2><span id="Prisoner's Dilemma">Prisoner's Dilemma</span></h2>
         <p>This is a round-based multiplayer Prisoner's Dilemma Game</p>
     </div>  
     </header>
-    <div class='for-button'> <div class='button' id='start_button'> Start Game </div> </div>
+    <div class='for-button'> <div class='button' id='start_button'> <p> Start Game </p> </div> </div>
     <script src="scripts.js"></script>
-    <div class='for-button'> <div class='button' id='beb'> Тык </div> </div>
+    <div class='for-button'> <div class='button' id='beb'> <p > Тык </p></div> </div>
     <script src="scripts.js"></script>
 </body>
 </html>
